911c8e6ebf957e8785c039c488a8c0228a5d8ace
Michael Thessel <michael.thessel@gmx.de>
2016-03-03 19:21:22 -1000

    Added support for app styles

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ gulp.task('sass', function () {
 });
 
 gulp.task('sass:watch', function () {
-  gulp.watch('styles/**/*.scss', ['sass']);
+  gulp.watch(['styles/**/*.scss', 'app/**/*.scss'], ['sass']);
 });
