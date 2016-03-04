0e9a466cc6c7a96f2bb3496b66ed4d6f5f2a5af7
Michael Thessel <michael.thessel@gmx.de>
2016-03-03 15:37:05 -1000

    Added home component scaffold

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,21 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {HomeComponent} from './home.component';
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html'
+    directives: [ROUTER_DIRECTIVES]
+    providers: [ROUTER_PROVIDERS]
 })
-export class AppComponent { }
+
+@RouteConfig([
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeComponent,
+        useAsDefasult: true,
+    }
+])
+
+export class AppComponent {}
---
+++ app/home.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-home',
+    templateUrl: 'app/home.component.html',
+})
+export class HomeComponent {}
