9501d3dccabda2f50d7b7d480d208ad4febc38a9
Michael Thessel <michael.thessel@gmx.de>
2016-03-05 10:38:52 -1000

    Minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
 
 var sassPaths = ['styles/style.scss'];
 
 gulp.task('sass', function () {
   return gulp.src(sassPaths, {base: './'})
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./'));
 });
 
