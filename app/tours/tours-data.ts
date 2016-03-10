59ad14148c2f25db794b21f75954aa20cc6c48e2
Michael Thessel <michael.thessel@gmx.de>
2016-03-09 17:39:12 -1000

    Added tour meta to tour page

--- app/tours/tours-data.ts
+++ app/tours/tours-data.ts
@@ -5,18 +5,21 @@ export var TOURS: Tour[] = [
       id: 1,
       slug: 'the-waikiki-experience',
       title: "Waikiki Beach ... clash of worlds",
+      description: "Tattooed intelligentsia organic chambray offal, pabst pinterest master cleanse cred. Austin freegan cornhole, street art meditation hella portland YOLO cardigan pitchfork everyday carry organic iPhone. Master cleanse taxidermy drinking vinegar echo park. Trust fund marfa pug, normcore ugh poutine humblebrag cred viral ennui schlitz.",
       place: "hawaii",
-      banner: {
-          image: 'media/home/header.jpg',
-      }
+      author: "Hawaian Ryan",
+      twitter: "Snowden",
+      instagram: "babypuggies",
+      web: "https://www.youtube.com/watch?v=QH2-TGUlwu4",
   },
   {
       id: 2,
       slug: 'hanauma-bay',
       title: "Hanauma Bay - Sacrifice to Tourism",
+      description: "Cronut hammock godard, semiotics squid forage butcher man bun",
       place: "hawaii",
-      banner: {
-          image: 'media/home/header.jpg',
-      }
+      author: "Pono Man",
+      twitter: "ggreenwald",
+      instagram: "cats_of_instagram",
   },
 ];
