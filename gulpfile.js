64f7df31b56f8bfb3daea5a050c567002081b57c
Michael Thessel <michael.thessel@gmx.de>
2016-03-14 11:25:19 -1000

    Added build steps to Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,69 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
+var typescript = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
+var tscConfig = require('./tsconfig.json');
 
 var sassPaths = ['styles/style.scss'];
 
-gulp.task('sass', function () {
-  return gulp.src(sassPaths, {base: './'})
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./'));
+/*
+ * Compile and minimize SCSS
+ */
+gulp.task('sass:compile', function () {
+    return gulp.src(sassPaths, {base: './'})
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./'));
 });
 
+/*
+ * Watch scss files for changes
+ */
 gulp.task('sass:watch', function () {
-  gulp.watch(['styles/**/*.scss', 'app/**/*.scss'], ['sass']);
+    return gulp.watch(['styles/**/*.scss', 'app/**/*.scss'], ['sass:compile']);
 });
+
+/*
+ * Clean build directory
+ */
+gulp.task('clean', function () {
+    return del('dist/**/*')
+});
+
+/*
+ * Compile Typescript
+ */
+gulp.task('ts:compile', ['clean'], function () {
+    return gulp
+        .src('app/**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tscConfig.compilerOptions))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/app'));
+});
+
+/*
+ * Copy libs
+ */
+gulp.task('copy:libs', ['clean'], function () {
+    return gulp
+        .src([
+            'node_modules/**/*'
+        ])
+        .pipe(gulp.dest('dist/node_modules'));
+});
+
+/*
+ * Copy assets
+ */
+gulp.task('copy:assets', ['clean'], function() {
+    return gulp
+        .src(['app/**/*', 'index.html', 'styles/style.css', '!app/**/*.ts', 'media/**/*' , 'fonts/**/*'], { base : './' })
+        .pipe(gulp.dest('dist'));
+});
+
+
+// Build the app for deployment
+gulp.task('build', ['sass:compile', 'ts:compile', 'copy:libs', 'copy:assets']);
