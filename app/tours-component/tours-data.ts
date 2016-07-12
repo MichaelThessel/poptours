8653de7c024ad08a05cfad0122e9b514166f3f73
Michael Thessel <michael.thessel@gmx.de>
2016-07-11 14:37:55 -1000

    Fixed encoding issue

--- app/tours-component/tours-data.ts
+++ app/tours-component/tours-data.ts
@@ -4,7 +4,7 @@ export var TOURS: Tour[] = [
   {
       id: 1,
       slug: "the-forgotten-history-of-kakaako",
-      title: "The Forgotten History of Kaka’ako ",
+      title: "The Forgotten History of Kaka'ako",
       description: "Kaka’ako was once once a thriving community with agricultural terraces, taro plantations, salt ponds and home to docks for foreign ships. Also, many ali’i called this place home. Today, Kaka’ako is one of the fastest growing neighborhoods in all of Oahu. There are several examples of places in Kaka’ako currently undergoing construction and development that can be seen by everyday pedestrians. These recent developments will likely price out many of the people who have called Kaka’ako home and make living their possible for only those who can afford it. Along with the rising cost of living, these new urban developments will destroy the cultural significance and history of Kaka’ako that was once present in the community.",
       place: "hawaii",
       author: "Aaron Falaniko",
